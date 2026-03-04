2a30c6659c159a79a3eedf492ac64a42b3eaa3e7
Alex-dev1356 <asoliven26@yahoo.com>
2026-03-05 05:50:28 +0800

    Registration of DBContext in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using PaymentAPI.Data;
+
 namespace PaymentAPI
 {
     public class Program
@@ -13,6 +16,9 @@ namespace PaymentAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<PaymentDetailContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
             var app = builder.Build();
 
