a697de78dd0736d4bd8345a086fc31db65218a02
Alex-dev1356 <asoliven26@yahoo.com>
2026-03-05 05:49:40 +0800

    Creation of PaymentDetailContext and Utilization of DBContext

---
+++ Data/PaymentDetailContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentAPI.Data
+{
+    public class PaymentDetailContext :DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Models.PaymentDetail> PaymentDetails { get; set; }
+    }
+}
